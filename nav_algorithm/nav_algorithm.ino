2a8ff95bfaeef8a84ef24c8cccbc1a106af608ce
Esteban Rodriguez Quintana <erquintana0805@gmail.com>
2022-12-03 19:16:47 -0600

    updating for fix

--- nav_algorithm/nav_algorithm.ino
+++ nav_algorithm/nav_algorithm.ino
@@ -16,17 +16,17 @@
 
 //GET_distances()
 //    leer utrasound1
-//        send 
+//        send
 //        delay
 //        recive
 //            calculo de distancia1
 //    leer utrasound2
-//        send 
+//        send
 //        delay
 //        recive
 //            calculo de distancia2
 //    leer utrasound3
-//        send 
+//        send
 //        delay
 //        recive
 //            calculo de distancia3
@@ -46,7 +46,7 @@
 //                motorA_B = 0
 //                motorB_F = 5
 //                motorB_B = 0
-//    else    
+//    else
 //        directo
 //
 //READ_TH()
@@ -56,163 +56,213 @@
 #include <Arduino_HTS221.h>
 #include <Arduino_LPS22HB.h>
 
-#define max_speed 1
-#define THRSHLD 0.25
-#define C_THRSHLD 0.15
+float max_speed = 1;
+float THRSHLD = 0.25;
+float C_THRSHLD = 0.1;
+int echoPinL = 2;  // attach pin D2 Arduino to pin Echo of HC-SR04
+int trigPinL = 3;  //attach pin D3 Arduino to pin Trig of HC-SR0int
+int echoPinC = 4;  // attach pin D2 Arduino to pin Echo of HC-SR04
+int trigPinC = 5;  //attach pin D3 Arduino to pin Trig of HC-SR0int
+int echoPinR = 6;  // attach pin D2 Arduino to pin Echo of HC-SR04
+int trigPinR = 7;  //attach pin D3 Arduino to pin Trig of HC-SR0int
 
-#define echoPinL 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPinL 3 //attach pin D3 Arduino to pin Trig of HC-SR04
-
-#define echoPinC 4 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPinC 5 //attach pin D3 Arduino to pin Trig of HC-SR04
-
-#define echoPinR 6 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPinR 7 //attach pin D3 Arduino to pin Trig of HC-SR04
-
-// motor pins:
-#define motor_R_F 8
-#define motor_R_B 9
-#define motor_L_F 10
-#define motor_L_B 11
+//motor pins:
+int motor_R_F = 8;
+int motor_R_B = 9;
+int motor_L_F = 10;
+int motor_L_B = 11;
 
 
-float AX,AY,AZ;
-float GX,GY,GZ;
-float MX,MY,MZ;
-float TEMP;     // ºC
-float HUM;      // %
-float PRES;     // kPa
-float PROX_C;   // cm   
-float PROX_L;   // cm
-float PROX_R;   // cm
-float VEL_RUEDA_I;
-float VEL_RUEDA_D;
-float POSE_X;
-float POSE_Y;
-int teleoperated;
+float AX, AY, AZ = 0;
+float GX, GY, GZ = 0;
+float MX, MY, MZ = 0;
+float TEMP = 0;    // ºC
+float HUM = 0;     // %
+float PRES = 0;    // kPa
+float PROX_C = 0;  // cm
+float PROX_L = 0;  // cm
+float PROX_R = 0;  // cm
+float VEL_RUEDA_I = 0;
+float VEL_RUEDA_D = 0;
+float POSE_X = 0;
+float POSE_Y = 0;
+int teleoperated = 0;
 
-void setup(){
-    //setting motors:
-    pinMode(motor_R_F, OUTPUT);
-    pinMode(motor_R_B, OUTPUT);
-    pinMode(motor_L_F, OUTPUT);
-    pinMode(motor_L_B, OUTPUT);
-    
-    Serial.begin(9600);
+float get_distance(int _trigPin, int _echoPin);
+void nav_algorithm(float max_speed, int trigPinL, int echoPinL, int trigPinC, int echoPinC, int trigPinR, int echoPinR);
 
-    if (!IMU.begin()) {
-        Serial.println("Failed to initialize IMU!");
-        while (1);
-    }
+void setup() {
+  //setting motors:
+  pinMode(motor_R_F, OUTPUT);
+  pinMode(motor_R_B, OUTPUT);
+  pinMode(motor_L_F, OUTPUT);
+  pinMode(motor_L_B, OUTPUT);
 
-    if (!HTS.begin()) {
-        Serial.println("Failed to initialize humidity temperature sensor!");
-        while (1);
+  Serial.begin(9600);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    //while (1);
+  }
+
+  if (!HTS.begin()) {
+    Serial.println("Failed to initialize humidity temperature sensor!");
+    //while (1);
   }
   if (!BARO.begin()) {
-        Serial.println("Failed to initialize pressure sensor!");
-        while (1);
+    Serial.println("Failed to initialize pressure sensor!");
+    //while (1);
   }
 }
 
 
-void loop(){
-    // execute navigation:
-    nav_algorithm(max_speed, trigPinL, echoPinL,trigPinC, echoPinC,trigPinR, echoPinR);
+void loop() {
+  float AX, AY, AZ;
+  float GX, GY, GZ;
+  float MX, MY, MZ;
+  float TEMP;    // ºC
+  float HUM;     // %
+  float PRES;    // kPa
+  float PROX_C;  // cm
+  float PROX_L;  // cm
+  float PROX_R;  // cm
+  float VEL_RUEDA_I;
+  float VEL_RUEDA_D;
+  float POSE_X;
+  float POSE_Y;
+  int teleoperated;
+  // execute navigation:
+  nav_algorithm(max_speed, trigPinL, echoPinL, trigPinC, echoPinC, trigPinR, echoPinR);
 
-    //getting Acceleration data:
-    if (IMU.accelerationAvailable()) {
-        IMU.readAcceleration(AX,AY,AZ);
+  //getting Acceleration data:
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(AX, AY, AZ);
+  }
+  //getting Gyro data:
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(GX, GY, GZ);
+  }
+  //getting Magnetic data:
+  if (IMU.magneticFieldAvailable()) {
+    IMU.readMagneticField(MX, MY, MZ);
+  }
+
+  //getting temperature/humidity data:
+  TEMP = BARO.readTemperature();
+  HUM = 10; //HTS.readHumidity();
+
+  //getting presure data:
+  PRES = BARO.readPressure();
+
+
+  
+  // Sending data to Bluetooth:
+  Serial.print(GX);
+  Serial.print(",");
+  Serial.print(GY);
+  Serial.print(",");
+  Serial.print(GZ);
+  Serial.print(",");
+  Serial.print(AX);
+  Serial.print(",");
+  Serial.print(AY);
+  Serial.print(",");
+  Serial.print(AZ);
+  Serial.print(",");
+  Serial.print(MX);
+  Serial.print(",");
+  Serial.print(MY);
+  Serial.print(",");
+  Serial.print(MZ);
+  Serial.print(",");
+  Serial.print(TEMP);
+  Serial.print(",");
+  Serial.print(HUM);
+  Serial.print(",");
+  Serial.print(PRES);
+  Serial.print(",");
+  Serial.print(PROX_C);
+  Serial.print(",");
+  Serial.print(PROX_L);
+  Serial.print(",");
+  Serial.print(PROX_R);
+  Serial.print(",");
+  Serial.print(VEL_RUEDA_I);
+  Serial.print(",");
+  Serial.print(VEL_RUEDA_D);
+  Serial.print(",");
+  Serial.print(POSE_X);
+  Serial.print(",");
+  Serial.print(POSE_Y);
+  Serial.print(",");
+  Serial.println(teleoperated);
+}
+
+void nav_algorithm(float max_speed, int trigPinL, int echoPinL, int trigPinC, int echoPinC, int trigPinR, int echoPinR) {
+  // getting distance data from ultrasounds:
+  PROX_L = 1;    //get_distance(trigPinL, echoPinL);
+  PROX_C = 0.2;  //get_distance(trigPinC, echoPinC);
+  PROX_R = 3;    //get_distance(trigPinR, echoPinR);
+
+  if (abs(PROX_L - PROX_R) > THRSHLD) {
+    if (PROX_L < PROX_R) {
+      drive_right(max_speed);
+      delay(1);
+    } else if (PROX_R < PROX_L) {
+      drive_left(max_speed);
+      delay(1);
     }
-    //getting Gyro data:
-    if (IMU.gyroscopeAvailable()) {
-        IMU.readGyroscope(GX,GY,GZ;);
+  } else {
+    if (PROX_C < C_THRSHLD) {
+      drive_backward(max_speed);
+      delay(1);
+    } else {
+      drive_forward(max_speed);
+      delay(1);
     }
-    //getting Magnetic data:
-    if (IMU.magneticFieldAvailable()) {
-        IMU.readMagneticField(MX,MY,MZ);
-    }
-    
-    //getting temperature/humidity data:
-    TEMP = HTS.readTemperature();
-    HUM  = HTS.readHumidity();
-
-    //getting presure data:
-    PRES = BARO.readPressure();
-    
-    // Sending data to Bluetooth:
-    Serial.println(GX,GY,GZ,AX,AY,AZ,MX,MY,MZ,TEMP,HUM,PRES,PROX_F,PROX_L,PROX_R,VEL_RUEDA_I,VEL_RUEDA_D,POSE_X,POSE_Y, teleoperated);
-    
-}
-void nav_algorithm(max_speed,trigPinL, echoPinL,trigPinC, echoPinC,trigPinR, echoPinR){
-    // getting distance data from ultrasounds:
-    PROX_L = get_distance(trigPinL, echoPinL);    
-    PROX_C = get_distance(trigPinC, echoPinC);
-    PROX_R = get_distance(trigPinR, echoPinR);    
-
-    if(abs(PROX_L-PROX_R)>THRSHLD){
-        if (PROX_L < PROX_R)
-        {
-            drive_right(max_speed);
-            delay(10);
-        }else if (PROX_R < PROX_L)
-        {
-             drive_left(max_speed);
-             delay(10);
-        }        
-    }else
-    {
-        if(PROX_C < C_THRSHLD){
-            drive_backward(max_speed);
-            delay(10);
-        }else
-        {
-            drive_forward(max_speed);
-            delay(10);
-        } 
-    }
+  }
 }
 
-void drive_forward(int value){
-    digitalWrite(motor_R_F, value);
-    digitalWrite(motor_R_B, 0);
-    digitalWrite(motor_L_F, value);
-    digitalWrite(motor_L_B, 0);
+void drive_forward(int value) {
+  digitalWrite(motor_R_F, value);
+  digitalWrite(motor_R_B, 0);
+  digitalWrite(motor_L_F, value);
+  digitalWrite(motor_L_B, 0);
 }
-void drive_backward(int value){
-    digitalWrite(motor_R_F, 0);
-    digitalWrite(motor_R_B, value);
-    digitalWrite(motor_L_F, 0);
-    digitalWrite(motor_L_B, value);
-}
-
-void drive_right(int value){
-    digitalWrite(motor_R_F, value/2);
-    digitalWrite(motor_R_B, 0);
-    digitalWrite(motor_L_F, value);
-    digitalWrite(motor_L_B, 0);
-}
-void drive_left(int value){
-    digitalWrite(motor_R_F, value);
-    digitalWrite(motor_R_B, 0);
-    digitalWrite(motor_L_F, value/2);
-    digitalWrite(motor_L_B, 0);
+void drive_backward(int value) {
+  digitalWrite(motor_R_F, 0);
+  digitalWrite(motor_R_B, value);
+  digitalWrite(motor_L_F, 0);
+  digitalWrite(motor_L_B, value);
 }
 
-float get_distance(int _trigPin, int _echoPin){
-    long duration; // variable for the duration of sound wave travel
-    int distance; // variable for the distance measurement
-    // Clears the _trigPin condition
-    digitalWrite(_trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the _trigPin HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(_trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(_trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(_echoPin, HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-    // Displays the distance on the Serial Monitor
-    return distance;
+void drive_right(int value) {
+  digitalWrite(motor_R_F, value / 2);
+  digitalWrite(motor_R_B, 0);
+  digitalWrite(motor_L_F, value);
+  digitalWrite(motor_L_B, 0);
+}
+void drive_left(int value) {
+  digitalWrite(motor_R_F, value);
+  digitalWrite(motor_R_B, 0);
+  digitalWrite(motor_L_F, value / 2);
+  digitalWrite(motor_L_B, 0);
+}
+
+float get_distance(int _trigPin, int _echoPin) {
+  long duration;  // variable for the duration of sound wave travel
+  int distance;   // variable for the distance measurement
+  // Clears the _trigPin condition
+  digitalWrite(_trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the _trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(_trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(_trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(_echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  return distance;
 }
